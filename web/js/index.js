49ff3b63f8d44e76d9ba93b2c1862db5718424b3
yanlli <sarahzhx@163.com>
2016-11-30 16:52:47 +0800

    完成index

---
+++ web/js/index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by yanlli on 2016/11/30.
+ */
+$(document).ready(function(){
+    $(".log_tips ul li").click(function () {
+        $(".log_tips ul li").removeClass("selected");
+        $(this).addClass("selected")
+    })
+    $("#confirm").click(function(){
+        var name=$("#name").val();
+        var password=$("#password").val();
+        if($(".selected").html()=="用户登录"){
+
+        }else{
+            var manager_url="/library/public/admin/login";
+        }
+        $.post(manager_url,{name:name,pwd:password},function(result){
+            console.log(result);
+        });
+    })
+})
+
