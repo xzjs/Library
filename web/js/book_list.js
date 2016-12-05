8785454cc7e079dc235e7aaf3d11598380eab972
yanlli <sarahzhx@163.com>
2016-12-05 21:21:20 +0800

    完善全选按钮

--- web/js/book_list.js
+++ web/js/book_list.js
@@ -2,37 +2,11 @@
  * Created by yanlli on 2016/12/2.
  */
 $(document).ready(function () {
-    var tr=$(".book_message1");
-    tr.mouseover(function () {
-        $(this).css("background-color","#e8e8e8");
-    });
-    tr.mouseout(function () {
-        $(this).css("background-color","#f9f9f9");
-    });
-    $(".check_all").click(function(){
-        $("[name=checkbox]:checkbox").attr("checked",this.checked);
-    });
-    $("[name=checkbox]:checkbox").click(function() {
-        var flag = true;
-        var option=$(this).parent().parent();
-        if(this.checked){
-            option.addClass("delete_option");
-        }else{
-            option.removeClass("delete_option");
-        }
 
-        $("[name=checkbox]:checkbox").each(function () {
-            if (!this.checked) {
-                flag = false;
-            }
-        });
-        $(".check_all").attr("checked", flag);
-    });
     $(".delete").click(function () {
         $(this).css("box-shadow","#5e5e5e 1px 1px 0px");
         $(".delete_option").each(function () {
-            console.log($(this));
-            $(this).remove();
+           $(this).remove();
         });
     });
     $(".add").click(function () {
@@ -42,15 +16,7 @@ $(document).ready(function () {
        var url=window.URL.createObjectURL(this.files.item(0));
         $(".preview").attr("src",url);
     });
-    $(".modification").mouseover(function () {
-        $(this).css({"width":"20px","height":"20px"})
-    });
-    $(".modification").mouseout(function () {
-        $(this).css({"width":"16px","height":"16px"})
-    });
-    $(".modification").click(function () {
 
-    });
     $("#save").click(function () {
         var formData=new FormData($("form")[0]);
         $.ajax({
@@ -60,8 +26,65 @@ $(document).ready(function () {
             processData:false,
             contentType:false,
             success:function (e) {
-                console.log(e);
+                if(e=='true'){
+                    $('#myModal').modal('hide');
+                    getBooksList();
+                }
             }
         })
     });
-})
+    getBooksList();
+});
+
+function getBooksList () {
+    $.getJSON("/library/public/book",function (result) {
+        var table=$("table");
+        $(".book_message1").remove();
+        for(var i=0;i<result.length;i++){
+            var html='<tr class="book_message1"><td><input name="checkbox" type="checkbox"> </td> <td>'+result[i]["id"]+'</td> <td>'+result[i]["book_no"]+'</td> <td>'+result[i]["name"]+'</td> <td>'+result[i]["author"]+'</td> <td>'+result[i]["publish"]+'</td> <td width="4%"><img class="modification" src="img/modify.png" alt=""></td> </tr>';
+            table.append(html);
+        }
+        var tr=$(".book_message1");
+        tr.mouseover(function () {
+            $(this).css("background-color","#e8e8e8");
+        });
+        tr.mouseout(function () {
+            $(this).css("background-color","#f9f9f9");
+        });
+        $(".check_all").click(function(){
+            var check_all=this.checked;
+            $("[name=checkbox]:checkbox")
+            if(check_all==true){
+                $("[name=checkbox]:checkbox").each(function () {
+
+                })attr("checked",true);
+            }else{
+                $("[name=checkbox]:checkbox").attr("checked",false);
+            };
+
+        });
+        $("[name=checkbox]:checkbox").click(function() {
+            var flag = true;
+            var option=$(this).parent().parent();
+            if(this.checked){
+                option.addClass("delete_option");
+            }else{
+                option.removeClass("delete_option");
+            }
+            $("[name=checkbox]:checkbox").each(function () {
+                if (!this.checked) {
+                    flag = false;
+                }
+            });
+            $(".check_all").attr("checked", flag);
+        });
+        $(".modification").mouseover(function () {
+            $(this).css({"width":"20px","height":"20px"})
+        });
+        $(".modification").mouseout(function () {
+            $(this).css({"width":"16px","height":"16px"})
+        });
+        $(".modification").click(function () {
+        });
+    });
+}
