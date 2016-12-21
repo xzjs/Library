c27b8b9c69cbb06a00e2cb1b96a1735af2cd9435
yanlli <sarahzhx@163.com>
2016-12-21 16:46:31 +0800

    添加完普通用户下载页

--- web/js/book_list.js
+++ web/js/book_list.js
@@ -3,20 +3,33 @@
  */
 $(document).ready(function () {
     var adminId=$.cookie("admin_id");
-    if(adminId==null){
+    var userId=$.cookie("user_id");
+    if(adminId==null&&userId==null){
         $(".bms").hide();
+        $(".unload").show();
         remainTime();
-    }else{
-        $(".unload").hide();
     }
-
-    $("#delete").mousedown(function () {
+    //普通用户登录
+    if(userId!=""){
+        console.log("普通用户登录");
+        $("#download").show();
+    }
+    //管理员登录
+    if(adminId!=""&&userId==""){
+        console.log("管理员登录");
+        $("#delete").show();
+        $("#add").show();
+        $("#download").show();
+    }
+    getBooksList();
+    var del=$("#delete");
+    del.mousedown(function () {
         $(this).css("box-shadow", "#5e5e5e 1px 1px 0px");
     });
-    $("#delete").mouseup(function () {
+    del.mouseup(function () {
         $(this).css("box-shadow", "#5e5e5e 1px 1px 2px");
     });
-    $("#delete").click(function () {
+    del.click(function () {
         var sure = confirm("是否确定要删除？");
         if (sure == true) {
             var success = true;
@@ -41,11 +54,29 @@ $(document).ready(function () {
             }
         }
     });
-    $("#add").mousedown(function () {
+    var add=$("#add");
+    add.mousedown(function () {
         $(this).css("box-shadow", "#5e5e5e 1px 1px 0px");
     });
-    $("#add").mouseup(function () {
+    add.mouseup(function () {
         $(this).css("box-shadow", "#5e5e5e 1px 1px 2px");
+    });
+    var download=$("#download");
+    download.mousedown(function () {
+        $(this).css("box-shadow", "#5e5e5e 1px 1px 0px");
+    });
+    download.mouseup(function () {
+        $(this).css("box-shadow", "#5e5e5e 1px 1px 2px");
+    });
+    download.click(function () {
+        $("[name=checkbox]:checkbox").each(function () {
+            if (this.checked==true){
+                $(this).each(function () {
+                    $(this).parent().siblings().find("a")[0].click();
+                    console.log($(this).parent().siblings().find("a")[0]);
+                });
+            }
+        });
     });
     $("#cover_file").change(function () {
         var url = window.URL.createObjectURL(this.files.item(0));
@@ -105,94 +136,104 @@ $(document).ready(function () {
             });
         }
     });
-    getBooksList();
+    $(".book_list").keydown(function () {
+        if (event.keyCode==13){
+            $(".search_button").click();
+        }
+    });
     $(".search_button").click(function(){
-        var type=$(":selected").val();
+        var type=$.trim($(":selected").val());
         var key=$(".search_text").val();
-        $.post("/library/public/book/search",{type:type,key:key},function (result) {
-            var table = $("table");
-            $(".book_message1").remove();
-            for (var i = 0; i < result.length; i++) {
-                var html = '<tr data-id=' + result[i]["id"] + ' data-img='
-                    + result[i]["img"] + ' data-content='
-                    + result[i]["content"] + ' class="book_message2"><td width="30px"><input name="checkbox" type="checkbox"> </td> <td width="30px">'
-                    + i + '</td> <td class="book_no" width="100px">'
-                    + result[i]["book_no"] + '</td> <td width="260px"><a class="name" href="/library/public/uploads/txts/' + result[i]["txt"] + '">'
-                    + result[i]["name"] + '</a></td> <td class="author" width="110px">'
-                    + result[i]["author"] + '</td> <td class="publish" width="120px">'
-                    + result[i]["publish"] + '</td> <td width="30px"><img data-toggle="modal" data-target="#myModal" class="modification" src="img/modify.png" alt=""></td> </tr>';
-                table.append(html);
-            }
-        },"json");
+        if (key!=""){
+            $.post("/library/public/book/search",{type:type,key:key},function (result) {
+                showBookList(result);
+            },"json");
+        }
+    });
+    $(".search_text").change(function () {
+        if($.trim($(this).val())==''){
+            getBooksList();
+        }else{
+            $(".search_button").click();
+        }
     });
 });
+function showBookList(result) {
+    var tHead = $(".book_list_table table");
+    $(".book_message1").remove();
+    for (var i = 0; i < result.length; i++) {
+        var html = '<tr data-id=' + result[i]["id"] + ' data-img='
+            + result[i]["img"] + ' data-content='
+            + result[i]["content"] + ' class="book_message1"><td width="30px"><input name="checkbox" type="checkbox"> </td> <td width="30px">'
+            + i + '</td> <td class="book_no" width="100px">'
+            + result[i]["book_no"] + '</td> <td width="260px"><a class="name" download href="/library/public/uploads/txts/' + result[i]["txt"] + '">'
+            + result[i]["name"] + '</a></td> <td class="author" width="110px">'
+            + result[i]["author"] + '</td> <td class="publish" width="120px">'
+            + result[i]["publish"]
+            + '</td> <td width="30px"><img data-toggle="modal" data-target="#myModal" class="modification" src="img/modify.png" alt=""></td></tr>';
+        tHead.append(html);
+    }
+    var tr = $(".book_message1");
+    tr.mouseover(function () {
+        $(this).css("background-color", "#e8e8e8");
+        $("#cover_img").attr("src", "/library/public/uploads/imgs/" + $(this).data("img"));
+        $("#content").html($(this).data("content"));
+    });
+    tr.mouseout(function () {
+        $(this).css("background-color", "#f9f9f9");
+    });
+    $(".check_all").click(function () {
+        var check_all = this.checked;
+        if (check_all == true) {
+            $("[name=checkbox]:checkbox").each(function () {
+                this.checked = true;
 
+            });
+        } else {
+            $("[name=checkbox]:checkbox").each(function () {
+                this.checked = false;
+            });
+        };
+    });
+    $("[name=checkbox]:checkbox").click(function () {
+        var flag = true;
+        $("[name=checkbox]:checkbox").each(function () {
+            if (!this.checked) {
+                flag = false;
+            }
+        });
+        $(".check_all").each(function () {
+            this.checked = flag;
+        })
+    });
+    $(".modification").mouseover(function () {
+        $(this).css({"width": "20px", "height": "20px"})
+    });
+    $(".modification").mouseout(function () {
+        $(this).css({"width": "16px", "height": "16px"})
+    });
+    var userId=$.cookie("user_id");
+    if(userId!=""){
+        $(".modification").hide();
+    }
+    $(".modification").click(function () {
+        var thisTr = $(this).parent().parent();
+        $("[name=book_no]").val(thisTr.children(".book_no").html());
+        $("[name=name]").val(thisTr.find(".name").html());
+        $("[name=author]").val(thisTr.children(".author").html());
+        $("[name=publish]").val(thisTr.children(".publish").html());
+        $("[name=content]").val(thisTr.data("content"));
+        $("#cover_file").val("");
+        $(".preview").attr("src","");
+        $("[name=txt]").val("");
+        var thisTrId = thisTr.data("id");
+        $("#book_id").val(thisTrId);
+
+    });
+}
 function getBooksList() {
     $.getJSON("/library/public/book", function (result) {
-        var table = $("table");
-        $(".book_message1").remove();
-        for (var i = 0; i < result.length; i++) {
-            var html = '<tr data-id=' + result[i]["id"] + ' data-img='
-                + result[i]["img"] + ' data-content='
-                + result[i]["content"] + ' class="book_message1"><td width="30px"><input name="checkbox" type="checkbox"> </td> <td width="30px">'
-                + i + '</td> <td class="book_no" width="100px">'
-                + result[i]["book_no"] + '</td> <td width="260px"><a class="name" href="/library/public/uploads/txts/' + result[i]["txt"] + '">'
-                + result[i]["name"] + '</a></td> <td class="author" width="110px">'
-                + result[i]["author"] + '</td> <td class="publish" width="120px">'
-                + result[i]["publish"] + '</td> <td width="30px"><img data-toggle="modal" data-target="#myModal" class="modification" src="img/modify.png" alt=""></td> </tr>';
-            table.append(html);
-        }
-        var tr = $(".book_message1");
-        tr.mouseover(function () {
-            $(this).css("background-color", "#e8e8e8");
-            $("#cover_img").attr("src", "/library/public/uploads/imgs/" + $(this).data("img"));
-            $("#content").html($(this).data("content"));
-        });
-        tr.mouseout(function () {
-            $(this).css("background-color", "#f9f9f9");
-        });
-        $(".check_all").click(function () {
-            var check_all = this.checked;
-            if (check_all == true) {
-                $("[name=checkbox]:checkbox").each(function () {
-                    this.checked = true;
-                });
-            } else {
-                $("[name=checkbox]:checkbox").each(function () {
-                    this.checked = false;
-                });
-            }
-            ;
-        });
-        $("[name=checkbox]:checkbox").click(function () {
-            var flag = true;
-            $("[name=checkbox]:checkbox").each(function () {
-                if (!this.checked) {
-                    flag = false;
-                }
-            });
-            $(".check_all").each(function () {
-                this.checked = flag;
-            })
-        });
-        $(".modification").mouseover(function () {
-            $(this).css({"width": "20px", "height": "20px"})
-        });
-        $(".modification").mouseout(function () {
-            $(this).css({"width": "16px", "height": "16px"})
-        });
-        $(".modification").click(function () {
-            var thisTr = $(this).parent().parent();
-            $("[name=book_no]").val(thisTr.children(".book_no").html());
-            $("[name=name]").val(thisTr.find(".name").html());
-            $("[name=author]").val(thisTr.children(".author").html());
-            $("[name=publish]").val(thisTr.children(".publish").html());
-            $("[name=content]").val(thisTr.data("content"));
-            var thisTrId = thisTr.data("id");
-            $("#book_id").val(thisTrId);
-
-
-        });
+        showBookList(result);
     });
 }
 var t=5;
--- web/js/index.js
+++ web/js/index.js
@@ -5,27 +5,82 @@ $(document).ready(function () {
     $(".log_tips ul li").click(function () {
         $(".log_tips ul li").removeClass("selected");
         $(this).addClass("selected");
-    })
+    });
     $("#confirm").click(function () {
         var name = $("#name").val();
         var password = $("#password").val();
         if (name != "" && password != "") {
-            if ($(".selected").html() == "用户登录") {
 
-            } else {
+            var user_id;
+            var admin_id;
+            if ($(".selected").html() == "用户登录") {
+                user_id=name;
+                admin_id="";
+                var manager_url ="/library/public/user/login";
+
+            } else if($(".selected").html() == "管理员登录"){
+                admin_id = name;
+                user_id="";
                 var manager_url = "/library/public/admin/login";
             }
             $.post(manager_url, {name: name, pwd: password}, function (result) {
                 if (result == 0) {
                     $(".tip").css("display", "block");
                 } else if (result > 0) {
-                    $.cookie("admin_id",result);
+                    $.cookie("admin_id", admin_id);
+                    $.cookie("user_id",user_id);
                     window.location.href = "book_list.html";
                 }
             });
         } else {
             alert("用户名或密码不能为空！");
         }
-    })
-})
+    });
+    $("#password").keydown(function () {
+        if (event.keyCode==13){
+            $("#confirm").click();
+        }
+    });
+    $("#save").click(function () {
+        var bossName=$("[name=boss_name]").val();
+        var name=$.trim($("[name=name]").val());
+        var pwd=$.trim($("[name=pwd]").val());
+        var pwd2=$.trim($("[name=pwd2]").val());
+        if (bossName!="张艳丽"){
+            $("#boss_wrong").css("display","inline-block");
+        }else{
+            $("#boss_wrong").css("display","none");
+        }
+        if (name==''){
+            $("#name_wrong").css("display","inline-block");
+        }else{
+            $("#name_wrong").css("display","none");
+        }
+        if (pwd==''){
+            $("#pwd_wrong").css("display","inline-block");
+        }else{
+            $("#pwd_wrong").css("display","none");
+        }
+        if (pwd2==''){
+            $("#pwd2_wrong").css("display","inline-block");
+        }else{
+            $("#pwd2_wrong").css("display","none");
+        }
+        if (pwd!=""&&pwd2!=""&&pwd!=pwd2){
+            $("#unequal").css("display","inline-block");
+        }else{
+            $("#unequal").css("display","none");
+        }
+        if (bossName=="张艳丽"&&name!=""&&pwd!=""&&pwd==pwd2){
+            $.post("/library/public/user",{name:name,pwd:pwd},function (result) {
+                console.log(result);
+                if (result!=0){
+                    alert("注册成功！点击确认进入图书馆");
+                    $.cookie("user_id", name);
+                    window.location.href = "book_list.html";
+                }
+            });
+        }
+    });
+});
 
